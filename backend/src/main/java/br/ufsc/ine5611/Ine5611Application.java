877ec973389791a3599d052590cf9517d1e36e76
Pedro Binotto <pedrosantibinotto@gmail.com>
2023-08-23 18:22:14 -0300

    first commit

---
+++ backend/src/main/java/br/ufsc/ine5611/Ine5611Application.java
@@ -0,0 +1,13 @@
+package br.ufsc.ine5611;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ine5611Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ine5611Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/ufsc/ine5611/Ine5611ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsc.ine5611;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ine5611ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
