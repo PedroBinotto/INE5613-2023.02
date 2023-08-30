3f965ca8a78177bbd13967fb9bd0d7255c6ec3a3
Pedro Binotto <pedrosantibinotto@gmail.com>
2023-08-30 12:50:09 -0300

    dataloader transactions

---
+++ dataloader/src/main/resources/transactions/1.0.0/trans_1.sql
@@ -0,0 +1,32 @@
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('João', 'Muniz', '59557485477');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('Fernanda', 'Ignacia', '64522261268');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('Lucimberto', 'Bernardes', '16420240409');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('Gláucio', 'Castilho', '94616128421');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('Luiz', 'Queiroz', '36666637291');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('José', 'Uchôa', '27405222153');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('Daniele', 'Zava', '07752621889');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('Cleyton', 'Conceição', '90751457213');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('Emanuel', 'Camelo', '80606623922');
+INSERT INTO tb_pessoa_fisica (nome, sobrenome, cpf) VALUES ('Tânia', 'Dias', '22999876904');
+
+INSERT INTO tb_funcionario (id_pessoa_fisica_funcionario) VALUES (1);
+INSERT INTO tb_funcionario (id_pessoa_fisica_funcionario) VALUES (2);
+INSERT INTO tb_funcionario (id_pessoa_fisica_funcionario) VALUES (3);
+INSERT INTO tb_funcionario (id_pessoa_fisica_funcionario) VALUES (4);
+INSERT INTO tb_funcionario (id_pessoa_fisica_funcionario) VALUES (5);
+
+INSERT INTO tb_cliente (id_pessoa_fisica_cliente) VALUES (6);
+INSERT INTO tb_cliente (id_pessoa_fisica_cliente) VALUES (7);
+INSERT INTO tb_cliente (id_pessoa_fisica_cliente) VALUES (8);
+INSERT INTO tb_cliente (id_pessoa_fisica_cliente) VALUES (9);
+
+INSERT INTO tb_estabelecimento (id_uf_estabelecimento, endereco_estabelecimento) VALUES (24, 'Lauro Linhares, 2050, Florianópolis');
+INSERT INTO tb_estabelecimento (id_uf_estabelecimento, endereco_estabelecimento) VALUES (24, 'Alameda Adolfo Konder, 1007, Florianópolis');
+INSERT INTO tb_estabelecimento (id_uf_estabelecimento, endereco_estabelecimento) VALUES (18, 'Brigadeiro Franco, 2575, Curitiba');
+INSERT INTO tb_estabelecimento (id_uf_estabelecimento, endereco_estabelecimento) VALUES (18, 'Av. Visconte de Guarapuava, Curitiba');
+
+INSERT INTO tb_produto (nome, valor) VALUES ('Furadeira Makita', 2499.99);
+INSERT INTO tb_produto (nome, valor) VALUES ('Chiclete Trindent', 3.99);
+INSERT INTO tb_produto (nome, valor) VALUES ('Marlboro Red', 9.99);
+INSERT INTO tb_produto (nome, valor) VALUES ('Água Minera Imperatriz', 1.99);
+INSERT INTO tb_produto (nome, valor) VALUES ('Brahma Duplo Malte', 4.49);
---
+++ dataloader/src/main/resources/transactions/1.0.0/trans_2.sql
@@ -0,0 +1,24 @@
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (1, 2, 1);
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (2, 1, 1);
+
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (1, 1, 2);
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (3, 2, 2);
+
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (1, 1, 3);
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (3, 2, 3);
+
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (3, 1, 4);
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (4, 2, 4);
+INSERT INTO tb_lotacao (id_lotacao_funcionario, id_lotacao_cargo, id_lotacao_estabelecimento) VALUES (5, 2, 4);
+
+INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (1, '48999424779');
+INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (1, '48999752827');
+INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (1, '48999212823');
+
+INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (2, '48999244123');
+
+INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (3, '48999224173');
+INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (3, '48999994378');
+
+INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (4, '48999194379');
+INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (4, '48999844378');
