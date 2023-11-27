ecac35386eac94fc1c1bb1526c87faae12b927a2
Pedro Binotto <pedrosantibinotto@gmail.com>
2023-11-27 16:39:47 -0300

    mock inserts de vendas no dataloader

--- dataloader/src/main/resources/transactions/1.0.0/trans_2.sql
+++ dataloader/src/main/resources/transactions/1.0.0/trans_2.sql
@@ -22,3 +22,18 @@ INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (3, '48
 
 INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (4, '48999194379');
 INSERT INTO tb_telefone (id_pessoa_fisica_telefone, num_telefone) VALUES (4, '48999844378');
+
+INSERT INTO tb_venda (
+    id_venda_funcionario,
+    id_venda_estabelecimento,
+    id_venda_cargo,
+    id_venda_cliente,
+    data_hora_venda
+) values (1, 1, 2, 1, now()),
+         (3, 2, 2, 1, now());
+
+INSERT INTO tb_rl_venda_produto (
+    id_rl_venda_produto_v,
+    id_rl_venda_produto_p,
+    quantidade
+) VALUES (1, 1, 1), (1, 2, 2), (1, 3, 1), (2, 4, 3), (2, 5, 3);
